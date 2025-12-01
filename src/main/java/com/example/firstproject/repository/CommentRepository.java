b8a8d8b4fb0fe434307903d8b21a76d2ecd3fcc3
이재훈 <jhl2211768@gmail.com>
2025-12-01 16:23:24 +0900

    test

---
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // 특정 게시글의 모든 댓글 조회
+    // 네이티브 쿼리 메서드를 만드는 방법1 = @Query 어노테이션 이용
+    @Query(value = "SELECT * FROM comment WHERE article_id = :articleId", nativeQuery = true)
+    List<Comment> findByArticleId(Long articleId);
+
+    // 특정 닉네임의 모든 댓글 조회
+    // 네이티브 쿼리 메서드를 만드는 방법2 - orm.xml 파일 이용
+    List<Comment> findByNickname(String nickname);
+}
